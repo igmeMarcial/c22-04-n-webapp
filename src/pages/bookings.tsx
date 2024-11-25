38c1aa8600c8260558499adfc56278b2006244cc
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-11-25 18:48:54 -0400

    Replace PetForm with BookingList and BookingForm components in Booking page

--- src/pages/bookings.tsx
+++ src/pages/bookings.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import PetForm from "@/components/PetForm";
+import BookingList from "@/components/BookingList";
+import BookingForm from "@/components/BookingForm";
 
 const Register: React.FC = () => {
   return (
     <div>
       <h1>Booking Page</h1>
-      <PetForm userId="c153c8fc-b306-48dd-87a9-9ec15756ee73" />
+
     </div>
   );
 };
