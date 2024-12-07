2b6c151f1843425e4ddc3b113be50d8e283e11a7
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-12-07 18:44:18 -0400

    feature: remove fixed width from modal in CaregiverProfileList for better responsiveness

--- src/components/caregiver-profiles/CaregiverProfileList.tsx
+++ src/components/caregiver-profiles/CaregiverProfileList.tsx
@@ -202,7 +202,7 @@ const CaregiversList = () => {
       {/* Modal */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white p-4 rounded-lg shadow-lg w-96">
+          <div className="bg-white p-4 rounded-lg shadow-lg ">
             <CreateBookingForm caregiver={selectedCaregiver} onClose={closeModal} />
           </div>
           </div>
