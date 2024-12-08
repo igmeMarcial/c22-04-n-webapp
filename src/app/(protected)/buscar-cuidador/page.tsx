e6cde4cb68ba866c89f878ec939f524e1c171b9b
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-12-07 20:56:24 -0400

    feature: add CareGiversPage component to display caregiver profiles

---
+++ src/app/(protected)/buscar-cuidador/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import CaregiverProfileList from "@/components/caregiver-profiles/CaregiverProfileList";
+
+const CareGiversPage: React.FC = () => {
+  return (
+
+        <CaregiverProfileList />
+
+  );
+};
+
+export default CareGiversPage;
